49121954ff2de731d2609fb25ed927416c858172
Tjorven Kämpfer <tjorven.kaempfer@gmail.com>
2022-07-06 13:59:14 +0200

    blynk setup

--- Eugen.ino
+++ Eugen.ino
@@ -1,8 +1,10 @@
+#include <BlynkSimpleEsp8266.h>           // Library to use the Blynk-App
+
 #include <WiFiManager.h>                  // Library to enable wifi controll
 WiFiManager wifiManager;
 WiFiClient client;
 
-
+char blynkAuth[] = "fW8dET98PxDO2kyxpZqzY03pjkUzjEuc";
 
 
 void setup() {
@@ -10,6 +12,8 @@ void setup() {
 
     // Also accessible at IP 192.168.4.1
   wifiManager.autoConnect("Eugen");      // Setup the name of the hotspot
+
+  Blynk.begin(blynkAuth, WiFi.SSID().c_str(), WiFi.psk().c_str(), "iot.informatik.uni-oldenburg.de", 8080);        // Connect to Blynk-Client
 
 }
 void loop() {
