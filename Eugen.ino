fd97b1d67106795789a9b22ddff763f4bbcf81e5
Tjorven Kämpfer <tjorven.kaempfer@gmail.com>
2022-07-09 13:05:26 +0200

    display connection status

--- Eugen.ino
+++ Eugen.ino
@@ -1,27 +1,30 @@
-#include <BlynkSimpleEsp8266.h>           // Library to use the Blynk-App
+#include <BlynkSimpleEsp8266.h> // Library to use the Blynk-App
 
-#include <WiFiManager.h>                  // Library to enable wifi controll
+#include <WiFiManager.h> // Library to enable wifi controll
 WiFiManager wifiManager;
 WiFiClient client;
 
-#include <SPI.h>                          // Library to controll the LED-Display
+#include <SPI.h> // Library to controll the LED-Display
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define OLED_RESET 0                       // "0" for ESP8266
+#include "Servo.h" // Library to controll the servo
+
+#define OLED_RESET 0 // "0" for ESP8266
 Adafruit_SSD1306 display(OLED_RESET);
 
 char blynkAuth[] = "fW8dET98PxDO2kyxpZqzY03pjkUzjEuc";
 
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);        // Initialize the OLED-Display
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Initialize the OLED-Display
   display.display();
 
-  display.clearDisplay();                           // Display connection status
+  display.clearDisplay(); // Display connection status
   display.setTextColor(WHITE);
   display.setTextSize(1);
   display.setCursor(0, 0);
@@ -31,15 +34,20 @@ void setup() {
   display.println("Eugen");
   display.setCursor(0, 0);
   display.display();
-  
+
   // Also accessible at IP 192.168.4.1
-  wifiManager.autoConnect("Eugen");      // Setup the name of the hotspot
+  wifiManager.autoConnect("Eugen"); // Setup the name of the hotspot
 
-  
+  display.clearDisplay(); // Display connection status
+  display.println("Verbunden");
+  display.setCursor(0, 0);
+  display.display();
 
-  Blynk.begin(blynkAuth, WiFi.SSID().c_str(), WiFi.psk().c_str(), "iot.informatik.uni-oldenburg.de", 8080);        // Connect to Blynk-Client
+  Blynk.begin(blynkAuth, WiFi.SSID().c_str(), WiFi.psk().c_str(), "iot.informatik.uni-oldenburg.de", 8080); // Connect to Blynk-Client
+}
+void loop()
+{
 
 }
-void loop() {
-  
-}
+
+
