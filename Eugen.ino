1649cba3b3af4a0bc8311391203cf11f95691ea6
Tjorven Kämpfer <tjorven.kaempfer@gmail.com>
2022-07-17 20:26:17 +0200

    documentation

--- Eugen.ino
+++ Eugen.ino
@@ -1,103 +1,100 @@
 #include <BlynkSimpleEsp8266.h> // Library to use the Blynk-App
-#define BLYNK_PRINT Serial
 
-#include <WiFiManager.h> // Library to enable wifi controll
-WiFiManager wifiManager;
-WiFiManagerParameter blynkToken("blynkToken", "Blynk Token", "", 33);
-WiFiClient client;
+#include <WiFiManager.h>                                              // Library to enable wifi controll
+WiFiManager wifiManager;                                              // Create a wifi manager object
+WiFiManagerParameter blynkToken("blynkToken", "Blynk Token", "", 33); // Parameter to set the Blynk Token
 
-#include <SPI.h> // Library to controll the LED-Display
+#include <SPI.h> // Libraries to controll the LED-Display
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
 #include "Servo.h" // Library to controll the servo
-Servo myservo;
+Servo myservo;     // Create a servo object
 
-#include "pitches.h" // Library contains the pitches of the notes
+#include "pitches.h" // Library contains pitches for notes
 
-#include <Hash.h> // Library to use the hash function
-#include <FS.h>   // Library to use the filesystem
+#include <FS.h> // Library to use the filesystem
 
 #define SERVO_PIN D7 // Data pin for the servo
 
 #define BUTTON_PIN D3              // Pin on which to read the button input
-const int SHORT_PRESS_TIME = 3000; // 3 seconds
+const int SHORT_PRESS_TIME = 3000; // 3 seconds is short press
 int lastState = HIGH;              // the previous state from the input pin
 int currentState;                  // the current reading from the input pin
-unsigned long pressedTime = 0;
-unsigned long releasedTime = 0;
+unsigned long pressedTime = 0;     // the time the button was pressed
+unsigned long releasedTime = 0;    // the time the button was released
 
-#define BUZZER_PIN D5 // Pinn on which the buzzer reads
+#define BUZZER_PIN D5 // Pin on which the buzzer reads
 
 #define OLED_RESET 0 // "0" for ESP8266
 Adafruit_SSD1306 display(OLED_RESET);
 
 boolean koffeePouring = false;
 
-String readFile(fs::FS &fs, const char *path)
+String readFile(fs::FS &fs, const char *path) // read the content of a file
 {
-  Serial.printf("Reading file: %s\r\n", path);
-  File file = fs.open(path, "r");
-  if (!file || file.isDirectory())
+  Serial.printf("Reading file: %s\r\n", path); // Debug message
+  File file = fs.open(path, "r");              // Open the file
+  if (!file || file.isDirectory())             // If the file doesn't exist or is a directory
   {
-    Serial.println("- empty file or failed to open file");
-    return String();
+    Serial.println("- empty file or failed to open file"); // Debug message
+    return String();                                       // Return an empty string
   }
-  Serial.println("- read from file:");
-  String fileContent;
-  while (file.available())
+  Serial.println("- read from file:"); // Debug message
+  String fileContent;                  // Create a string to hold the content of the file
+  while (file.available())             // While there is data to read
   {
-    fileContent += String((char)file.read());
+    fileContent += String((char)file.read()); // Add the character to the string
   }
-  file.close();
-  Serial.println(fileContent);
-  return fileContent;
+  file.close();                // Close the file
+  Serial.println(fileContent); // Debug message
+  return fileContent;          // Return the content of the file
 }
 
-void writeFile(fs::FS &fs, const char *path, const char *message)
+void writeFile(fs::FS &fs, const char *path, const char *message) // write to a file
 {
-  Serial.printf("Writing file: %s\r\n", path);
-  File file = fs.open(path, "w");
-  if (!file)
+  Serial.printf("Writing file: %s\r\n", path); // Debug message
+  File file = fs.open(path, "w");              // Open the file
+  if (!file)                                   // If the file doesn't exist
   {
-    Serial.println("- failed to open file for writing");
+    Serial.println("- failed to open file for writing"); // Debug message
     return;
   }
-  if (file.print(message))
+  if (file.print(message)) // Write the message to the file
   {
-    Serial.println("- file written");
+    Serial.println("- file written"); // Debug message
   }
   else
   {
-    Serial.println("- write failed");
+    Serial.println("- write failed"); // Debug message
   }
-  file.close();
+  file.close(); // Close the file
 }
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(9600); // Start the serial communication
 
-  myservo.attach(SERVO_PIN);
-  digitalWrite(SERVO_PIN, LOW);
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(BUZZER_PIN, OUTPUT);
+  myservo.attach(SERVO_PIN);         // Attach the servo to the pin
+  pinMode(BUTTON_PIN, INPUT_PULLUP); // Set the button pin to input with pullup
+  pinMode(BUZZER_PIN, OUTPUT);       // Set the buzzer pin to output
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Initialize the OLED-Display
-  display.display();
-  delay(2000);        // display startup animation
+  display.display();                         // display startup animation
+  delay(2000);
   if (SPIFFS.begin()) // Mount the file system
   {
     Serial.println("SPIFFS mounted"); // Debug message
   }
   else
   {
-    Serial.println("SPIFFS mount failed");
-    display.clearDisplay(); // Display the error message
+    Serial.println("SPIFFS mount failed"); // Debug message
+    display.clearDisplay();                // Display the error message
     display.setTextColor(WHITE);
     display.setTextSize(1);
-    display.println("SPIFFS mount failed");
+    display.setCursor(0, 0);
+    display.println("SPIFFS mount failed"); // Display the error message
     display.display();
     return; // Stop the program, if the file system could not be mounted
   }
@@ -113,42 +110,50 @@ void setup()
   display.setCursor(0, 0);
   display.display();
 
-  // Also accessible at IP 192.168.4.1
-  String tokenString = readFile(SPIFFS, "/blynkToken.txt");
+  String tokenString = readFile(SPIFFS, "/blynkToken.txt"); // Read the token from the file
   if (!tokenString.isEmpty())
   {
-    blynkToken.setValue(tokenString.c_str(), 33);
+    blynkToken.setValue(tokenString.c_str(), 33); // If a token was found, set the token to the html-page input
   }
-  wifiManager.addParameter(&blynkToken);
+  wifiManager.addParameter(&blynkToken); // Add the token as parameter to the wifi manager
 
-  wifiManager.autoConnect("Eugen");                            // Setup the name of the hotspot
+  // Also accessible at IP 192.168.4.1
+  wifiManager.autoConnect("Eugen");                            // Setup the name of the hotspot and connect to wifi
   writeFile(SPIFFS, "/blynkToken.txt", blynkToken.getValue()); // Write the blynk token to the filesystem
 
-  Blynk.connectWiFi(WiFi.SSID().c_str(), WiFi.psk().c_str()); // Connect to the wifi network
   Blynk.config(blynkToken.getValue(), "iot.informatik.uni-oldenburg.de", 8080);
-  bool connection = Blynk.connect(10000); // Connect to the Blynk server with a timeout of 10 seconds
+  display.clearDisplay(); // Display the connection status
+  display.setCursor(0, 0);
+  display.println("Verbindung");
+  display.println("zu Blynk");
+  display.println("wird hergestellt");
+  display.display();
+  bool connection = Blynk.connect(15000); // Connect to the Blynk server with a timeout of 15 seconds
 
-  if (!connection)
+  if (!connection) // If the no connection to the Blynk app could be established
   {
     display.clearDisplay(); // Display the error message
-    display.setTextColor(WHITE);
-    display.setTextSize(1);
+    display.setCursor(0, 0);
     display.println("Verbindungsfehler");
     display.display();
     delay(2000);
-    if (Blynk.isTokenInvalid()) // Check if the token is valid
+    if (Blynk.isTokenInvalid()) // Check if the token was invalid
     {
-      Serial.println("Invalid token, please check your Blynk App");
-      display.clearDisplay(); // Display connection status
+      Serial.println("Invalid token, please check your Blynk Token"); // Debug message
+      display.clearDisplay();                                         // Display connection status
       display.setCursor(0, 0);
       display.println("Token");
       display.println("ungueltig");
-      display.println("Geraet wird");
-      display.println("neugestartet");
       display.display();
       delay(5000);
     }
-    wifiManager.erase(); //Erase credentials from the filesystem if the connection failed
+    wifiManager.erase(); // Erase credentials from the filesystem if the connection failed
+    display.clearDisplay();
+    display.setCursor(0, 0);
+    display.println("Geraet wird"); // Display message about restarting the device
+    display.println("neugestartet");
+    display.display();
+    delay(5000);
     ESP.restart(); // Restart the program, if the connection could not be established
   }
 
@@ -175,8 +180,8 @@ void loop()
   if (lastState == HIGH && currentState == LOW) // button is pressed
     pressedTime = millis();
   else if (lastState == LOW && currentState == HIGH)
-  { // button is released
-    releasedTime = millis();
+  {
+    releasedTime = millis(); // button is released
 
     long pressDuration = releasedTime - pressedTime;
 
@@ -214,23 +219,21 @@ void pourCoffee()
   myservo.write(150);
   delay(1000);
 
-  digitalWrite(SERVO_PIN, LOW);
-
-  display.clearDisplay(); // Display that the controler is ready for the next coffee
+  display.clearDisplay(); // Display that the controller is ready for the next coffee
   display.setCursor(0, 0);
   display.println("Bereit");
   display.println("einen Kaffee");
   display.println("zu machen");
   display.display();
-  ;
+
   koffeePouring = false;
 
-  tone(BUZZER_PIN, NOTE_C4, 500); // Play a tone to indicate that the coffee is poured
+  tone(BUZZER_PIN, NOTE_C4, 500); // Play a tone to indicate that the coffee was poured
 }
 
 BLYNK_WRITE(V0) // Coffee button in blynk app is pressed
 {
-  int i = param.asInt();
+  int i = param.asInt(); // Get the value of the button,  1 is pour coffee, 0 is do nothing
   if (i == 0 || koffeePouring)
     return;
 
